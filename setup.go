05999d4f91c8e0b0268e9769997575eb0abcb762
1gtm <1gtm@appscode.com>
2021-07-22 03:08:43 +0000

    Generate code for provider@v1.1.1-0.20210721135909-47bb95202dd9 gen@625c7cd
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- setup.go
+++ setup.go
@@ -78,8 +78,8 @@ var runningControllers = struct {
 
 func watchCRD(ctx context.Context, crdClient *clientset.Clientset, vwcClient *admissionregistrationv1.AdmissionregistrationV1Client, stopCh <-chan struct{}, mgr manager.Manager, auditor *auditlib.EventPublisher, watchOnlyDefault bool) error {
 	informerFactory := informers.NewSharedInformerFactory(crdClient, time.Second*30)
-	i := informerFactory.Apiextensions().V1beta1().CustomResourceDefinitions().Informer()
-	l := informerFactory.Apiextensions().V1beta1().CustomResourceDefinitions().Lister()
+	i := informerFactory.Apiextensions().V1().CustomResourceDefinitions().Informer()
+	l := informerFactory.Apiextensions().V1().CustomResourceDefinitions().Lister()
 
 	i.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
